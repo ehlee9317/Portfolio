8c6653aa77ee5e3aa9bed982749fbc38fdd61fac
Eun Hyuk Lee <ehlee9317@gmail.com>
2021-02-19 01:19:16 -0500

    added carousel img

--- src/components/myCarousel/myCarousel.js
+++ src/components/myCarousel/myCarousel.js
@@ -1,27 +1,29 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import slide1 from "../../assets/slide1.jpg";
+import slide2 from "../../assets/slide2.jpg";
+import slide3 from "../../assets/slide3.jpg";
+
+import "./myCarousel.css";
 
 const MyCarousel = () => {
   return (
-    <>
-      <Carousel>
+    <div>
+      <Carousel
+        controls={false}
+        indicators
+        interval={3000}
+        pauseOnHover={false}
+      >
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
+          <img className="d-block w-100 custom-img" src={slide1} alt="First slide" />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
+          <img className="d-block w-100 custom-img" src={slide2} alt="Second slide" />
 
           <Carousel.Caption>
             <h3>Second slide label</h3>
@@ -29,11 +31,7 @@ const MyCarousel = () => {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
+          <img className="d-block w-100 custom-img" src={slide3} alt="Third slide" />
 
           <Carousel.Caption>
             <h3>Third slide label</h3>
@@ -43,7 +41,7 @@ const MyCarousel = () => {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-    </>
+    </div>
   );
 };
 
