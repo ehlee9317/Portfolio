ce7570417e9e6743bfbd3eab6f966872aa9523a0
Eun Hyuk Lee <ehlee9317@gmail.com>
2021-02-22 00:59:15 -0500

    changed carousel component

--- src/components/myCarousel/myCarousel.js
+++ src/components/myCarousel/myCarousel.js
@@ -4,6 +4,10 @@ import slide1 from "../../assets/slide1.jpg";
 import slide2 from "../../assets/slide2.jpg";
 import slide3 from "../../assets/slide3.jpg";
 
+import ScrollDown from "../scrollDown"
+
+
+
 import "./myCarousel.css";
 
 const MyCarousel = () => {
@@ -16,31 +20,32 @@ const MyCarousel = () => {
         pauseOnHover={false}
       >
         <Carousel.Item>
-          <img className="d-block w-100 custom-img" src={slide1} alt="First slide" />
-          <Carousel.Caption>
+          <img className="d-block w-100 custom-img" src={slide2} alt="First slide" />
+          {/* <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+          </Carousel.Caption> */}
         </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100 custom-img" src={slide2} alt="Second slide" />
+        {/* <Carousel.Item>
+          <img className="d-block w-100 custom-img" src={gif} alt="Second slide" /> */}
 
-          <Carousel.Caption>
+          {/* <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100 custom-img" src={slide3} alt="Third slide" />
+          </Carousel.Caption> */}
+        {/* </Carousel.Item> */}
+        {/* <Carousel.Item>
+          <img className="d-block w-100 custom-img" src={gif} alt="Third slide" /> */}
 
-          <Carousel.Caption>
+          {/* <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+          </Carousel.Caption> */}
+        {/* </Carousel.Item> */}
       </Carousel>
+      <ScrollDown />
     </div>
   );
 };
