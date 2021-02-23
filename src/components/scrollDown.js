9d607d8748f7a1599dcae15d52d0d66dc5e1ae2b
Eun Hyuk Lee <ehlee9317@gmail.com>
2021-02-23 16:55:17 -0500

    deleted material UI in scrollDown and replace with bootstrap

--- src/components/scrollDown.js
+++ src/components/scrollDown.js
@@ -1,18 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import Button from "@material-ui/core/Button"
+import Button from "react-bootstrap/Button";
 
 const MyScroll = styled.div`
   position: relative;
   z-index: 1;
   bottom: 20.6em;
-//   top: -10%
   display: flex;
-//   width: 100%;
   justify-content: center;
   .turn {
     cursor: pointer;
-   //  transform: rotate(90deg);
   }
 `;
 
@@ -20,7 +17,9 @@ const ScrollDown = () => {
   return (
     <MyScroll>
       <div className="turn">
-        <Button variant="outlined">View my work</Button>
+        <Button variant="dark" href="#about">
+          View My Work
+        </Button>
       </div>
     </MyScroll>
   );
