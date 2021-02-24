94d40177ec2ec727c8553ae7683565ce524f8fd5
Eun Hyuk Lee <ehlee9317@gmail.com>
2021-02-24 11:12:35 -0500

    code clean up for contact page

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,15 +1,11 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import Nav from "react-bootstrap/Nav";
 
 // ICONS
 import { FaLinkedinIn } from "react-icons/fa";
-import { FaFacebookF } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import { FaMediumM } from "react-icons/fa";
 import { BiChevronsUp } from "react-icons/bi";
 
 const Contact = () => {
@@ -17,12 +13,12 @@ const Contact = () => {
     <div id="contact">
       <div className="contact">
         <h1 className="pt-3 text-center font-details pb-3">CONTACT</h1>
-        <h4 className="pt-3 text-center pb-3">
-          Have a question or want to work together?
-        </h4>
 
-        <form class="w-50 ">
-          <div class="form-group">
+        <form class="w-50 container">
+          <div class="pt-3 pb-4 text-center">
+            <text>Have a question or want to work together?</text>
+          </div>
+          <div class="pb-1">
             <input
               type="name"
               class="form-control"
@@ -31,7 +27,7 @@ const Contact = () => {
             />
           </div>
 
-          <div class="form-group">
+          <div class="pb-1">
             <input
               type="email"
               class="form-control"
@@ -40,7 +36,7 @@ const Contact = () => {
             />
           </div>
 
-          <div class="form-group">
+          <div>
             <textarea
               class="form-control"
               id="exampleFormControlTextarea1"
@@ -48,21 +44,27 @@ const Contact = () => {
               placeholder="Your Message"
             ></textarea>
           </div>
+          <div class="text-right pt-1">
+            <button type="button" class="btn btn-outline-dark">
+              Submit
+            </button>
+            {/* <Button>SUBMIT</Button> */}
+          </div>
         </form>
-        <div className="text-right">
-          <Button class="btn btn-outline-dark">SUBMIT</Button>
-        </div>
         <div className="text-center pt-5">
-          <Button href="home">
+          <Button href="home" class="btn btn-secondary">
             <BiChevronsUp size={32} />
           </Button>
         </div>
 
         <div className="text-center pt-5 pb-5">
           <FaLinkedinIn size={32} />
-          <FaFacebookF size={30} />
           <FaInstagram size={33} />
           <FaGithub size={32} />
+          <FaMediumM size={30} />
+        </div>
+        <div class="text-center">
+          <text>EUN HYUK LEE ©2021</text>
         </div>
       </div>
     </div>
