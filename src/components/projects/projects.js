f65843a86190e59f493c2a1a4cec0dd0d1e1def6
Eun Hyuk Lee <ehlee9317@gmail.com>
2021-02-26 23:07:23 -0500

    fixed project sizes

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -28,26 +28,27 @@ const Projects = () => {
       return (
         <div>
           <CardDeck className="justify-content-center">
-            <Row>
-              <Col md={6}>
-                <Card>
+            <Row className="justify-content-center">
+              <Col md={3} className="pb-4 imageCard">
+                <Card className="mt-2 mb-2 ">
                   <Image
                     src={project.image}
                     alt={project.title}
                     rounded
-                    // className="image-style m-1"
+
                   ></Image>{" "}
                 </Card>
               </Col>
-              <Col md={6}>
-                <Card>
-                  <Card.Body>
-                    <Card.Title>
-                      {project.title} <h4>{project.caption}</h4>
+              <Col md={3} className="pb-4">
+                <Card className="mt-2 mb-2">
+                  <Card.Body >
+                    <Card.Title className="card-title">
+                      {project.title}
+                      <h4>{project.caption}</h4>
                     </Card.Title>
-                    <Card.Text>
                       <hr />
-                      <h4>{project.description}</h4>
+                    <Card.Text >
+                      <h4 className="infoText">{project.description}</h4>
                     </Card.Text>
                   </Card.Body>
                   <Accordion.Toggle
@@ -135,79 +136,6 @@ const Projects = () => {
       <div className="d-flex justify-content-between flex-column mt-1">
         <div>
           {displayProjects(projectLists)}
-          {/* <Accordion>
-            <Card>
-              <Accordion.Toggle
-                as={Card.Header}
-                eventKey="0"
-                className="p-2 text-center accordian-main"
-              >
-                PROJECT DETAILS
-              </Accordion.Toggle>
-
-              <Accordion.Collapse eventKey="0" className="text-left">
-                <Card.Body>
-                  <strong>Description:</strong> This is a Todo App created with
-                  React and Material UI that keeps a track of your Todos
-                  <hr />
-                  <strong>Features:</strong>
-                  <ul className="list-styles pt-1">
-                    <li>Keeps track of your Todos</li>
-                    <li>Powered by React and Material UI</li>
-                    <li>Respoisive Design</li>
-                  </ul>
-                  <hr />
-                  <strong>Tech used:</strong>
-                  <ul>
-                    <li>
-                      <span className="p-2">
-                        <Image
-                          src={L_HTML5}
-                          alt="HTML 5"
-                          rounded
-                          className="image-style m-1"
-                        ></Image>{" "}
-                        HTML5
-                      </span>
-                    </li>
-                    <li>
-                      <span className="p-2">
-                        <Image
-                          src={L_CSS}
-                          alt="CSS 3"
-                          rounded
-                          className="image-style m-1"
-                        ></Image>{" "}
-                        CSS3
-                      </span>
-                    </li>
-                    <li>
-                      <span className="p-2">
-                        <Image
-                          src={L_REACT}
-                          alt="React"
-                          rounded
-                          className="image-style1 m-1"
-                        ></Image>{" "}
-                        React
-                      </span>
-                    </li>
-                    <li>
-                      <span className="p-2">
-                        <Image
-                          src={L_MATERIALUI}
-                          alt="Material-UI"
-                          rounded
-                          className="image-style1 m-1"
-                        ></Image>{" "}
-                        Material-UI
-                      </span>
-                    </li>
-                  </ul>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-          </Accordion> */}
         </div>
       </div>
     </div>
