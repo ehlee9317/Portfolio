6d638d2e0ead89722c6377bce959aba2b49a21da
Eun Hyuk Lee <ehlee9317@gmail.com>
2021-02-28 00:34:48 -0500

    added animation to the view my work button

--- src/components/scrollDown.js
+++ src/components/scrollDown.js
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "react-bootstrap/Button";
+import { GrLinkNext } from "react-icons/gr";
+import { GrLinkDown } from "react-icons/gr";
+
+import "./scrollDown.css";
 
 const MyScroll = styled.div`
   position: relative;
@@ -17,8 +21,8 @@ const ScrollDown = () => {
   return (
     <MyScroll>
       <div className="turn">
-        <Button variant="dark" href="#about">
-          View My Work
+        <Button variant="outline-info" href="#about" className="button">
+          <span>View My Work</span>
         </Button>
       </div>
     </MyScroll>
